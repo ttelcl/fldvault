536b8e7c27144c15eb349a9a468728d109bcd18b
ttelcl <9429819+ttelcl@users.noreply.github.com>
2023-06-04 17:40:44 +0300

    Splitting ZVLT specific block type constants into a separate Zvlt2BlockType class

--- FldVault.Core/Vaults/Model/BlockType.cs
+++ FldVault.Core/Vaults/Model/BlockType.cs
@@ -12,7 +12,8 @@ using System.Threading.Tasks;
 namespace FldVault.Core.Vaults.Model
 {
   /// <summary>
-  /// Constants used as block types in ZVLT files
+  /// Constants used as block types in generic block files
+  /// (including ZVLT V2 files)
   /// </summary>
   public static class BlockType
   {
@@ -31,31 +32,6 @@ namespace FldVault.Core.Vaults.Model
     /// Plain unauthenticated comment string ('UCMT')
     /// </summary>
     public const int UnauthenticatedComment = 0x544D4355;
-
-    /// <summary>
-    /// ZVLT file header block 'Zvlt'
-    /// </summary>
-    public const int ZvltFile = 0x746C665A;
-
-    /// <summary>
-    /// File name block ('FNAM')
-    /// </summary>
-    public const int FileName = 0x4D414E46;
-
-    /// <summary>
-    /// Large file element header block ('FLX(')
-    /// </summary>
-    public const int FileHeader = 0x28584C46;
-
-    /// <summary>
-    /// Large file first content block ('FCT1')
-    /// </summary>
-    public const int FileContent1 = 0x31544346;
-
-    /// <summary>
-    /// Large file later content block ('FCTn')
-    /// </summary>
-    public const int FileContentN = 0x6E544346;
 
   }
 }
---
+++ FldVault.Core/Zvlt2/Zvlt2BlockType.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * (c) 2023  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FldVault.Core.Zvlt2;
+
+/// <summary>
+/// Block types used in ZVLT v2 files
+/// </summary>
+public static class Zvlt2BlockType
+{
+
+  /// <summary>
+  /// ZVLT file header block ('Zvlt')
+  /// </summary>
+  public const int ZvltFile = 0x746C665A;
+
+  /// <summary>
+  /// Embedded *.pass.key-info file ('PASS')
+  /// </summary>
+  public const int PassphraseLink = 0x53534150;
+
+  /// <summary>
+  /// File name block ('FNAM')
+  /// </summary>
+  public const int FileName = 0x4D414E46;
+
+  /// <summary>
+  /// Large file element header block ('FLX(')
+  /// </summary>
+  public const int FileHeader = 0x28584C46;
+
+  /// <summary>
+  /// Large file first content block ('FCT1')
+  /// </summary>
+  public const int FileContent1 = 0x31544346;
+
+  /// <summary>
+  /// Large file later content block ('FCTn')
+  /// </summary>
+  public const int FileContentN = 0x6E544346;
+
+}

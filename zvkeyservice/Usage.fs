319c242d62ae7e26ebf7d03609c37a9784b6d434
ttelcl <9429819+ttelcl@users.noreply.github.com>
2023-08-04 21:48:56 +0300

    preparations for adding "zvlt key-serve" functionality

--- zvkeyservice/Usage.fs
+++ zvkeyservice/Usage.fs
@@ -7,10 +7,10 @@ open ColorPrint
 let usage detail =
   cp "\foZVLT key server\f0 (pseudo-service)"
   cp ""
-  cp "\fozvkeyservice run\f0 [options]"
+  cp "\fozvkeyservice serve\f0 [\fg-s \fc<path>\f0] [\fg-F\f0]"
   cp " Run the key service"
   cp " \fg-s \fc<path>\f0   Use a non-standard socket for communication"
-  cp " \fg-F\f0\fx          (or \fg-force\f0) Try to replace a seemingly running server"
+  cp " \fg-F\f0\fx          (or \fg-force\f0) Try to replace a broken server"
   cp ""
   cp "\fyCommon options"
   cp " \fg-v               \f0Verbose mode"
---
+++ zvlt/KeyServer.fs
@@ -0,0 +1,25 @@
+﻿module KeyServer
+
+// functionality related to zvlt.exe <-> zvkeyservice communication
+
+open System
+open System.IO
+open System.Runtime.ExceptionServices
+open System.Threading
+
+open FldVault.Core
+open FldVault.Core.Crypto
+open FldVault.Core.Vaults
+open FldVault.Core.Zvlt2
+open FldVault.KeyServer
+
+open UdSocketLib.Communication
+open UdSocketLib.Framing
+open UdSocketLib.Framing.Layer1
+
+open ColorPrint
+open CommonTools
+
+let uploadKey (keyBuffer: KeyBuffer) =
+  failwith "NYI"
+  ()

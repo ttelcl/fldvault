f83cbb4a9a4c709f91d49d86280cbfe3393ebba1
ttelcl <9429819+ttelcl@users.noreply.github.com>
2023-12-26 17:53:29 +0200

    Gui: now using MahApps.Metro

--- ZvaultKeyServer/App.xaml.cs
+++ ZvaultKeyServer/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Windows;
 
 namespace ZvaultKeyServer;
@@ -9,4 +10,21 @@ namespace ZvaultKeyServer;
 /// </summary>
 public partial class App: Application
 {
+  /// <summary>
+  /// Instead of using a Startup Uri, create the window manually.
+  /// This method is referenced in the header of app.xaml instead of
+  /// a startup URI.
+  /// </summary>
+  private void App_Startup(object sender, StartupEventArgs e)
+  {
+    Trace.TraceInformation($"App.App_Startup enter");
+    var mainWindow = new MainWindow();
+    //MainModel = new MainViewModel();
+    //mainWindow.DataContext = MainModel;
+    Trace.TraceInformation($"App.App_Startup showing main window");
+    mainWindow.Show();
+    Trace.TraceInformation($"App.App_Startup exit");
+  }
+
+  //public MainViewModel? MainModel { get; private set; }
 }
--- ZvaultKeyServer/MainWindow.xaml.cs
+++ ZvaultKeyServer/MainWindow.xaml.cs
@@ -9,12 +9,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using MahApps.Metro.Controls;
+
 namespace ZvaultKeyServer;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow: Window
+public partial class MainWindow: MetroWindow
 {
   public MainWindow()
   {

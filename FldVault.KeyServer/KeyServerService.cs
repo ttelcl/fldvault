d16df5c570cc87d56ad4e222395ea72f8d59ab63
ttelcl <9429819+ttelcl@users.noreply.github.com>
2024-11-17 22:16:57 +0200

    Starting to add asynchronous versions of zvault client APIs (WIP)

--- FldVault.KeyServer/KeyServerService.cs
+++ FldVault.KeyServer/KeyServerService.cs
@@ -54,7 +54,10 @@ public class KeyServerService
 
   /// <summary>
   /// Try to synchronously get the key from the key server. Fails if the key is not
-  /// present or if there is no key server.
+  /// present or if there is no key server. Clients use this to synchronously
+  /// (and blocking) retrieve a key from the server when they are not interested
+  /// in telling the server what file the key is for. Use <see cref="RegisterFileSync"/>
+  /// for the case where the server should associate the key with a file.
   /// On success, the key is inserted in <paramref name="keyChain"/>.
   /// </summary>
   /// <param name="keyId">
@@ -101,8 +104,60 @@ public class KeyServerService
   }
 
   /// <summary>
+  /// Asynchronously look up a key in the key server. This is the async version of
+  /// <see cref="LookupKeySync"/>. The key is inserted in <paramref name="keyChain"/>
+  /// if successful.
+  /// </summary>
+  /// <param name="keyId">
+  /// The ID of the key to look up
+  /// </param>
+  /// <param name="keyChain">
+  /// The keychain where the key is stored if found.
+  /// </param>
+  /// <param name="cancellationToken">
+  /// Cancellation token
+  /// </param>
+  /// <returns>
+  /// True if the key was found, false if not. An exception is thrown in
+  /// error conditions.
+  /// </returns>
+  public async Task<bool> LookupKeyAsync(
+    Guid keyId, KeyChain keyChain, CancellationToken cancellationToken)
+  {
+    if(!ServerAvailable)
+    {
+      return false;
+    }
+    using(var client = await SocketService.ConnectClientAsync(cancellationToken))
+    {
+      var frameOut = new MessageFrameOut();
+      frameOut.WriteKeyRequest(keyId);
+      await client.SendFrameAsync(frameOut, cancellationToken);
+      var frameIn = new MessageFrameIn();
+      var receiveOk = await client.TryFillFrameAsync(frameIn, cancellationToken);
+      if(!receiveOk)
+      {
+        return false;
+      }
+      var messageCode = frameIn.MessageCode();
+      switch(messageCode)
+      {
+        case KeyServerMessages.KeyNotFoundCode:
+          return false;
+        case KeyServerMessages.KeyResponseCode:
+          frameIn.ReadKeyResponse(keyChain);
+          return true;
+        default:
+          throw new InvalidOperationException(
+            $"Unexpected response from server: 0x{messageCode:X08}");
+      }
+    }
+  }
+
+  /// <summary>
   /// Register a *.zvlt or *.pass.key-info file in the server, associating it
-  /// with its key. Also looks up that key if available (adding it to the key chain)
+  /// with its key. Also looks up that key if available (adding it to the key chain).
+  /// To look up a key by ID, use <see cref="LookupKeySync"/> instead.
   /// </summary>
   /// <param name="fileName">
   /// The name of the file to register
@@ -145,6 +200,58 @@ public class KeyServerService
         case KeyServerMessages.KeyResponseCode:
           frameIn.ReadKeyResponse(keyChain);
           return true;
+        case MessageCodes.ErrorText:
+          var error = frameIn.ReadError() ?? "Unknown error";
+          throw new InvalidOperationException(
+            $"Server rejected the registration: {error}");
+        default:
+          throw new InvalidOperationException(
+            $"Unexpected response from server: 0x{messageCode:X08}");
+      }
+    }
+  }
+
+  /// <summary>
+  /// Asynchronously register a vault file or key info file in the server,
+  /// returning the key for it into the key chain if known.
+  /// </summary>
+  /// <param name="fileName">
+  /// The name of the file to register
+  /// </param>
+  /// <param name="keyChain">
+  /// The key chain that will receive the key if it is found.
+  /// </param>
+  /// <param name="cancellationToken"></param>
+  /// <returns>
+  /// If not found (or if the server is not available), returns null.
+  /// On success, returns the key ID.
+  /// </returns>
+  /// <exception cref="InvalidOperationException"></exception>
+  public async Task<Guid?> RegisterFileAsync(
+    string fileName, KeyChain keyChain, CancellationToken cancellationToken)
+  {
+    if(!ServerAvailable)
+    {
+      return null;
+    }
+    using(var client = await SocketService.ConnectClientAsync(cancellationToken))
+    {
+      var frameOut = new MessageFrameOut();
+      frameOut.WriteKeyForFileRequest(fileName);
+      await client.SendFrameAsync(frameOut, cancellationToken);
+      var frameIn = new MessageFrameIn();
+      var receiveOk = await client.TryFillFrameAsync(frameIn, cancellationToken);
+      if(!receiveOk)
+      {
+        return null;
+      }
+      var messageCode = frameIn.MessageCode();
+      switch(messageCode)
+      {
+        case KeyServerMessages.KeyNotFoundCode:
+          return null;
+        case KeyServerMessages.KeyResponseCode:
+          return frameIn.ReadKeyResponse(keyChain);
         case MessageCodes.ErrorText:
           var error = frameIn.ReadError() ?? "Unknown error";
           throw new InvalidOperationException(

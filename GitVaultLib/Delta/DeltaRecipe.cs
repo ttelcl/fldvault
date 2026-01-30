ed64b4af32520e68f85f5e4f183f3eaa1d524b20
ttelcl <9429819+ttelcl@users.noreply.github.com>
2026-01-30 17:26:49 +0200

    Initial delta bundle recipes support. Not put to use yet.

---
+++ GitVaultLib/Delta/DeltaRecipe.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace GitVaultLib.Delta;
+
+/// <summary>
+/// A named recipe for a delta bundle.
+/// </summary>
+public class DeltaRecipe
+{
+  private readonly List<string> _seeds;
+  private readonly List<string> _exclusions;
+
+  /// <summary>
+  /// Create a new <see cref="DeltaRecipe"/>. Also used as JSON constructor.
+  /// </summary>
+  /// <param name="name">
+  /// The recipe name
+  /// </param>
+  /// <param name="seeds">
+  /// The initial content for <see cref="Seeds"/>.
+  /// </param>
+  /// <param name="exclusions">
+  /// The initial content for <see cref="Exclusions"/>.
+  /// </param>
+  public DeltaRecipe(string name, IEnumerable<string> seeds, IEnumerable<string> exclusions)
+  {
+    Name = name;
+    _seeds = seeds.ToList();
+    _exclusions = exclusions.ToList();
+    Seeds = _seeds.AsReadOnly();
+    Exclusions = _exclusions.AsReadOnly();
+  }
+
+  /// <summary>
+  /// The name identifying this recipe
+  /// </summary>
+  [JsonProperty("name")]
+  public string Name { get; }
+
+  /// <summary>
+  /// Specifies one or more refs or options to identify seed commits. These can name
+  /// branches (optionally prefixed with "heads/"), tags (optionally prefixed with
+  /// "tags/"). Alternatively these can be the options to include a group of seeds:
+  /// "--branches", "--tags", "--all". Note that commit IDs are not valid here.
+  /// </summary>
+  [JsonProperty("seeds")]
+  public IReadOnlyList<string> Seeds { get; }
+
+  /// <summary>
+  /// Specifies zero or more refs that are assumed to be known and are excluded from
+  /// the delta bundle. These can be branches or tags or commit IDs. If empty the resulting
+  /// bundle will be a full bundle instead of a delta bundle.
+  /// </summary>
+  [JsonProperty("exclusions")]
+  public IReadOnlyList<string> Exclusions { get; }
+
+  /// <summary>
+  /// A flag set when <see cref="Zap"/>, <see cref="AddSeed(string)"/> or <see cref="AddExclusion(string)"/>
+  /// is called.
+  /// </summary>
+  [JsonIgnore]
+  public bool Modified { get; internal set; }
+
+  /// <summary>
+  /// Remove a seed or exclusion by name
+  /// </summary>
+  /// <param name="seedOrExclusion"></param>
+  /// <returns></returns>
+  public bool Zap(string seedOrExclusion)
+  {
+    Modified = true;
+    // Assume it exists in either seeds or exclusions, but not both
+    return _seeds.Remove(seedOrExclusion) || _exclusions.Remove(seedOrExclusion);
+  }
+
+  /// <summary>
+  /// Add a seed or pseudo-seed
+  /// </summary>
+  /// <param name="seed"></param>
+  /// <exception cref="ArgumentException"></exception>
+  public void AddSeed(string seed)
+  {
+    if(seed.StartsWith("--") && !(seed=="--all" || seed=="--branches" || seed=="--tags"))
+    {
+      throw new ArgumentException(
+        $"Not a valid pseudo-seed name: '{seed}'",
+        nameof(seed));
+    }
+    Modified = true;
+    _exclusions.Remove(seed); // just in case
+    _seeds.Add(seed);
+  }
+
+  /// <summary>
+  /// Add an exclusion
+  /// </summary>
+  /// <param name="exclusion"></param>
+  public void AddExclusion(string exclusion)
+  {
+    Modified = true;
+    _seeds.Remove(exclusion); // just in case
+    _exclusions.Add(exclusion);
+  }
+}
---
+++ GitVaultLib/Delta/DeltaRecipes.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GitVaultLib.GitThings;
+
+using Newtonsoft.Json;
+
+namespace GitVaultLib.Delta;
+
+/// <summary>
+/// A collection of <see cref="DeltaRecipe"/> instances, indexed by name
+/// </summary>
+public class DeltaRecipes
+{
+  private readonly Dictionary<string, DeltaRecipe> _recipes;
+
+  /// <summary>
+  /// Create a new <see cref="DeltaRecipes"/> instance
+  /// </summary>
+  /// <param name="recipes"></param>
+  public DeltaRecipes(
+    IReadOnlyDictionary<string, DeltaRecipe> recipes)
+  {
+    _recipes = new Dictionary<string, DeltaRecipe>(recipes);
+  }
+
+  /// <summary>
+  /// Try to load the delta recipes file if it exists, returning null if not found.
+  /// The file not existing may mean that the repo is not set up for GitVault use
+  /// at all, or that no recipes were ever defined.
+  /// </summary>
+  /// <param name="gitRepoFolder"></param>
+  /// <returns></returns>
+  public static DeltaRecipes? TryLoad(GitRepoFolder gitRepoFolder)
+  {
+    if(File.Exists(gitRepoFolder.GitvaultRecipesFile))
+    {
+      var json = File.ReadAllText(gitRepoFolder.GitvaultRecipesFile);
+      return JsonConvert.DeserializeObject<DeltaRecipes>(json);
+    }
+    return null;
+  }
+
+  /// <summary>
+  /// A mapping from recipe name to recipe instance
+  /// </summary>
+  [JsonProperty("recipes")]
+  public IReadOnlyDictionary<string, DeltaRecipe> Recipes => _recipes;
+
+  /// <summary>
+  /// A flag set when a recipe is added, replaced or removed. Not set when
+  /// recipes are changed!
+  /// is called.
+  /// </summary>
+  [JsonIgnore]
+  public bool Modified { get; private set; }
+
+  /// <summary>
+  /// Add or replace a recipe
+  /// </summary>
+  /// <param name="recipe"></param>
+  public void Put(DeltaRecipe recipe)
+  {
+    Modified = true;
+    _recipes[recipe.Name] = recipe;
+  }
+
+  /// <summary>
+  /// Remove a recipe
+  /// </summary>
+  /// <param name="recipeName"></param>
+  /// <returns></returns>
+  public bool Drop(string recipeName)
+  {
+    if(_recipes.Remove(recipeName))
+    {
+      Modified = true;
+      return true;
+    }
+    return false;
+  }
+
+  /// <summary>
+  /// Save this instance if modified
+  /// </summary>
+  /// <param name="repoFolder"></param>
+  /// <returns></returns>
+  public bool SaveIfModified(
+    GitRepoFolder repoFolder)
+  {
+    var modified = Modified || _recipes.Values.Any(r => r.Modified);
+    if(modified)
+    {
+      var json = JsonConvert.SerializeObject(this, Formatting.Indented);
+      var file = repoFolder.GitvaultRecipesFile;
+      File.WriteAllText(file, json);
+      Modified = false;
+      foreach(var recipe in _recipes.Values)
+      {
+        recipe.Modified = false;
+      }
+    }
+    return modified;
+  }
+
+}

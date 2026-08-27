9c0cb0e4bd381422794839b71d86ef23a25a615d
ttelcl <9429819+ttelcl@users.noreply.github.com>
2026-08-27 23:27:27 +0300

    Initial TaskTabBaseViewModel. Known bug: "IsActive" logic is currently INCORRECT.

--- KeyLoader/Main/MainViewModel.cs
+++ KeyLoader/Main/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,8 @@ using System.Windows.Input;
 
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+
+using KeyLoader.Main.TaskTab;
 
 namespace KeyLoader.Main;
 
@@ -26,6 +30,7 @@ public class MainViewModel: ObservableObject
       var w = Application.Current.MainWindow;
       w?.Close();
     });
+    TaskTabs = new ObservableCollection<TaskTabBaseViewModel>();
   }
 
   /// <summary>
@@ -47,4 +52,49 @@ public class MainViewModel: ObservableObject
   }
   private string _statusMessage = "CellGrids demo application (work in progress)";
 
+  /// <summary>
+  /// The list of open Task Tabs (implemented by subclasses of <see cref="TaskTabBaseViewModel"/>)
+  /// </summary>
+  public ObservableCollection<TaskTabBaseViewModel> TaskTabs { get; }
+
+  /// <summary>
+  /// The currently active Task Tab, if any.
+  /// The tab to add should already be present in <see cref="TaskTabs"/>.
+  /// </summary>
+  public TaskTabBaseViewModel? CurrentTab {
+    get => _currentTab;
+    set {
+      if(value != null && !TaskTabs.Contains(value))
+      {
+        TaskTabs.Add(value);
+        Trace.TraceWarning(
+          $"Setting unknown tab '{value.Title}' as active tab. Adding it as new tab as side effect.");
+      }
+      if(value != _currentTab)
+      {
+        var old = _currentTab;
+        OnPropertyChanging();
+        if(old != null)
+        {
+          // In this case the callback from setting old.IsActive fizzles because
+          // it no longer is the current tab
+          old.IsActive = false; // this will set _currentTab to null
+        }
+        if(value != null)
+        {
+          // In this case the callback from setting new.IsActive fizzles because
+          // it already is the current tab
+          value.IsActive = true;
+        }
+        OnPropertyChanged();
+      }
+      throw new NotImplementedException("This logic is incorrect!!!");
+    }
+  }
+  private TaskTabBaseViewModel? _currentTab;
+
+  internal void SetCurrentTabCallback(TaskTabBaseViewModel? currentTab) 
+  {
+    _currentTab = currentTab;
+  }
 }
---
+++ KeyLoader/Main/TaskTab/TaskTabBaseViewModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace KeyLoader.Main.TaskTab;
+
+/// <summary>
+/// Shared base class for all task tab implementations
+/// </summary>
+public class TaskTabBaseViewModel: ObservableObject, IDisposable
+{
+  private bool _disposed;
+
+  /// <summary>
+  /// Create a new <see cref="TaskTabBaseViewModel"/>
+  /// </summary>
+  /// <param name="owner"></param>
+  /// <param name="title">
+  /// The title of the tab
+  /// </param>
+  /// <param name="modified">
+  /// True if the tab initially is in the "modified" state.
+  /// </param>
+  public TaskTabBaseViewModel(
+    MainViewModel owner,
+    string title = "untitled",
+    bool modified = false)
+  {
+    Owner = owner;
+    _title = title;
+    _modified = modified;
+  }
+
+  /// <summary>
+  /// The owning <see cref="MainViewModel"/>
+  /// </summary>
+  public MainViewModel Owner { get; }
+
+  /// <summary>
+  /// The tab title
+  /// </summary>
+  public string Title {
+    get => _title;
+    set {
+      SetProperty(ref _title, value);
+    }
+  }
+  private string _title;
+
+  /// <summary>
+  /// True if this tab has been modified and thus can not be closed without further interaction
+  /// </summary>
+  public bool Modified {
+    get => _modified;
+    protected set {
+      SetProperty(ref _modified, value);
+    }
+  }
+  private bool _modified;
+
+  /// <summary>
+  /// Get or set if this tab is the current one. Changing this
+  /// affects <see cref="MainViewModel.CurrentTab"/>.
+  /// </summary>
+  public bool IsActive {
+    get => this == Owner.CurrentTab;
+    set {
+      if(this == Owner.CurrentTab && !value)
+      {
+        OnPropertyChanging();
+        Owner.CurrentTab = null;
+        OnPropertyChanged();
+      }
+      else if(this != Owner.CurrentTab && value)
+      {
+        OnPropertyChanging();
+        Owner.CurrentTab = this;
+        OnPropertyChanged();
+      }
+    }
+  }
+
+  /// <summary>
+  /// Clean up (dispose pattern). This base implementation does nothing.
+  /// </summary>
+  /// <param name="disposing"></param>
+  protected virtual void Dispose(bool disposing)
+  {
+    if(!_disposed)
+    {
+      _disposed=true;
+      if(disposing)
+      {
+        // TODO: dispose managed state (managed objects)
+      }
+    }
+  }
+
+  /// <summary>
+  /// Clean up. Redirects to <see cref="Dispose(bool)"/>.
+  /// </summary>
+  public void Dispose()
+  {
+    // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    Dispose(disposing: true);
+    GC.SuppressFinalize(this);
+  }
+}

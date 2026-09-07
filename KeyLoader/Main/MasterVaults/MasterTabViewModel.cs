5f454445f78ba6a8b01fc8980925c243d184c74c
ttelcl <9429819+ttelcl@users.noreply.github.com>
2026-09-07 21:48:19 +0300

    KeyLoader: adding New and Open buttons in the control area

--- KeyLoader/Main/MasterVaults/MasterTabViewModel.cs
+++ KeyLoader/Main/MasterVaults/MasterTabViewModel.cs
@@ -66,7 +66,7 @@ public class MasterTabViewModel: TaskTabBaseViewModel
     }
     else if(State == MasterTabState.AwaitingKey)
     {
-      // TODO
+      EnterVaultPassEntry.PasswordTask?.TryFocus();
     }
   }
 

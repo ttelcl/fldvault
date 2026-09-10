6f2931f1cbc9e8a22df16a04151830167b277600
ttelcl <9429819+ttelcl@users.noreply.github.com>
2026-09-10 22:56:00 +0300

    Initial MasterVaultViewModel and ChildKeyViewModel. Untested and probably incomplete

---
+++ KeyLoader/Main/MasterVaults/ChildKeyViewModel.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using FldVault.Core.Vaults;
+using FldVault.KeyServer;
+
+namespace KeyLoader.Main.MasterVaults;
+
+/// <summary>
+/// ViewModel for a single child key inside a <see cref="MasterVaultViewModel"/>.
+/// The only thing guaranteed to exist is the child key ID. Both the
+/// raw key and the key info may be missing. The raw key itself is not actually stored
+/// here, only a flag indicating its availability.
+/// </summary>
+public class ChildKeyViewModel: ObservableObject
+{
+  /// <summary>
+  /// Create a new <see cref="ChildKeyViewModel"/>
+  /// </summary>
+  /// <param name="vaultModel"></param>
+  /// <param name="keyId"></param>
+  internal ChildKeyViewModel(
+    MasterVaultViewModel vaultModel,
+    Guid keyId)
+  {
+    VaultModel = vaultModel;
+    KeyId = keyId;
+    TryLoadKeyCommand = new AsyncRelayCommand(
+      () => TryLoadKey(),
+      () => !KeyKnown && VaultModel.Owner.Owner.KeyServer.ServerAvailable);
+    UpdateKeyKnown();
+  }
+
+  /// <summary>
+  /// The owning <see cref="MasterVaultViewModel"/> that this child
+  /// is part of
+  /// </summary>
+  public MasterVaultViewModel VaultModel { get; }
+
+  /// <summary>
+  /// The ID of the key that this object represents.
+  /// </summary>
+  public Guid KeyId { get; }
+
+  /// <summary>
+  /// Whether or not the raw key is known. This is a cached copy,
+  /// set by calling <see cref="UpdateKeyKnown"/>.
+  /// </summary>
+  public bool KeyKnown {
+    get => _keyKnown;
+    private set {
+      SetProperty(ref _keyKnown, value);
+    }
+  }
+  private bool _keyKnown;
+
+  /// <summary>
+  /// Get or set the key info (passphrase link) for this key
+  /// </summary>
+  public PassphraseKeyInfoFile? KeyInfo {
+    get => _keyInfo;
+    internal set {
+      if(value != null && value.KeyId != KeyId)
+      {
+        throw new InvalidOperationException(
+          "The key ids of the passphrase link and this key viewmodel do not match");
+      }
+      SetProperty(ref _keyInfo, value);
+    }
+  }
+  private PassphraseKeyInfoFile? _keyInfo;
+
+  /// <summary>
+  /// Try to load the raw key from the server, if it is still missing and the
+  /// server is available
+  /// </summary>
+  public AsyncRelayCommand TryLoadKeyCommand { get; }
+
+  /// <summary>
+  /// Updates the value of <see cref="KeyKnown"/> to its correct value
+  /// </summary>
+  public void UpdateKeyKnown()
+  {
+    KeyKnown = VaultModel.HasChildKey(KeyId);
+  }
+
+  private async Task TryLoadKey()
+  {
+    UpdateKeyKnown();
+    if(KeyKnown)
+    {
+      return;
+    }
+    var result = await VaultModel.TryRetrieveKey(KeyId);
+    if(result == null)
+    {
+      MessageBox.Show(
+        "The key server is not available",
+        "Failed");
+    }
+    else
+    {
+      switch(result.Value)
+      {
+        case KeyPresence.Unavailable:
+          MessageBox.Show(
+            "The key is not available in the server",
+            "Failed");
+          return;
+        case KeyPresence.Cloaked:
+          MessageBox.Show(
+            "The key is present but hidden in the server. Consider unhiding it.",
+            "Failed");
+          return;
+        case KeyPresence.Present:
+          // success
+          UpdateKeyKnown();
+          return;
+        default:
+          MessageBox.Show(
+            "Internal error. Unrecognized server response.",
+            "Failed");
+          return;
+      }
+    }
+  }
+}
---
+++ KeyLoader/Main/MasterVaults/MasterVaultViewModel.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using FldVault.Core.Crypto;
+using FldVault.Core.Vaults;
+using FldVault.KeyServer;
+
+namespace KeyLoader.Main.MasterVaults;
+
+/// <summary>
+/// ViewModel for an unlocked master key vault and its contents.
+/// </summary>
+public class MasterVaultViewModel: ObservableObject
+{
+  /// <summary>
+  /// A copy of <see cref="Owner"/>'s child key chain
+  /// </summary>
+  private readonly KeyChain _childKeyChain;
+  private readonly Dictionary<Guid, ChildKeyViewModel> _children;
+
+  /// <summary>
+  /// Create a new <see cref="MasterVaultViewModel"/>
+  /// </summary>
+  /// <param name="owner">
+  /// The owning <see cref="MasterTabViewModel"/> providing the details
+  /// of the vault
+  /// </param>
+  /// <param name="childKeyChain">
+  /// The child key chain from <paramref name="owner"/>
+  /// </param>
+  public MasterVaultViewModel(
+    MasterTabViewModel owner,
+    KeyChain childKeyChain)
+  {
+    Owner = owner;
+    _childKeyChain = childKeyChain;
+    Keys = new ObservableCollection<ChildKeyViewModel>();
+    _children = new Dictionary<Guid, ChildKeyViewModel>();
+    if(!Owner.FileExists)
+    {
+      throw new InvalidOperationException(
+        "Cannot create the MasterVaultViewModel: the file does not exist");
+    }
+    if(!Owner.MasterKeyLoaded)
+    {
+      throw new InvalidOperationException(
+        "Cannot create the MasterVaultViewModel: the key is not available");
+    }
+    ReloadContent();
+  }
+
+  /// <summary>
+  /// The owner of this unlocked master vault viewmodel, providing the details
+  /// of the vault that are not related to the fact that the vault is unlocked,
+  /// as well as the parts that assert it is unlocked.
+  /// </summary>
+  public MasterTabViewModel Owner { get; }
+
+  /// <summary>
+  /// The list child keys in this vault, as a list. This is an MVVM friendly
+  /// copy of the list in the key map.
+  /// </summary>
+  public ObservableCollection<ChildKeyViewModel> Keys { get; }
+
+  /// <summary>
+  /// Get or create a <see cref="ChildKeyViewModel"/> for the key
+  /// indicated by <paramref name="keyId"/>
+  /// </summary>
+  /// <param name="keyId"></param>
+  /// <returns></returns>
+  public ChildKeyViewModel GetKey(Guid keyId)
+  {
+    if(!_children.TryGetValue(keyId, out var childVm))
+    {
+      childVm = new ChildKeyViewModel(this, keyId);
+      _children.Add(keyId, childVm);
+      Keys.Add(childVm);
+    }
+    return childVm;
+  }
+
+  /// <summary>
+  /// Add a key to this list. Refreshes the key-known state. If necessary
+  /// this creates the <see cref="ChildKeyViewModel"/> for the key first.
+  /// </summary>
+  /// <param name="keyId"></param>
+  public ChildKeyViewModel AddKey(Guid keyId)
+  {
+    var vm = GetKey(keyId);
+    vm.UpdateKeyKnown();
+    return vm;
+  }
+
+  /// <summary>
+  /// Set the <see cref="ChildKeyViewModel.KeyInfo"/> for the child key
+  /// indicated by <see cref="PassphraseKeyInfoFile.KeyId"/>, adding a new
+  /// <see cref="ChildKeyViewModel"/> first if necessary.
+  /// </summary>
+  /// <param name="pkif"></param>
+  /// <returns></returns>
+  public ChildKeyViewModel AddKey(PassphraseKeyInfoFile pkif)
+  {
+    var vm = GetKey(pkif.KeyId);
+    vm.KeyInfo = pkif;
+    return vm;
+  }
+
+  /// <summary>
+  /// Try to get the <see cref="ChildKeyViewModel"/> for the given <paramref name="keyId"/>
+  /// </summary>
+  /// <param name="keyId">
+  /// The ID to look up
+  /// </param>
+  /// <param name="childVm">
+  /// Receives the value if found. May be null otherwise
+  /// </param>
+  /// <returns>
+  /// True if found, false if unknown.
+  /// </returns>
+  public bool TryFindKey(Guid keyId, [MaybeNullWhen(false)] out ChildKeyViewModel childVm)
+  {
+    return _children.TryGetValue(keyId, out childVm);
+  }
+
+  /// <summary>
+  /// Asynchronously refresh the raw key value from the key server, if 
+  /// the key server is available.
+  /// </summary>
+  /// <param name="keyId"></param>
+  /// <returns></returns>
+  public async Task<KeyPresence?> TryRetrieveKey(Guid keyId)
+  {
+    var serverWidget = Owner.Owner.ServerWidget;
+    var server = serverWidget.Server;
+    if(server.ServerAvailable)
+    {
+      var result = await server.LookupKeyAsync(keyId, _childKeyChain, serverWidget.AppCancelationToken);
+      if(result == KeyPresence.Present)
+      {
+        var vm = GetKey(keyId);
+        vm.UpdateKeyKnown();
+      }
+      return result;
+    }
+    return null;
+  }
+
+  internal bool HasChildKey(Guid keyId)
+  {
+    return _childKeyChain.ContainsKey(keyId);
+  }
+
+  private void ReloadContent()
+  {
+    // not yet implemented
+  }
+}
